c09ae8e783217d44abfe8a23d054dda38112f575
Platty114 <jamesplatt0@gmail.com>
2024-11-14 10:48:50 -0700

    first 4 cwes

---
+++ example_vulnerabilities/cwe-1234.sv
@@ -0,0 +1,41 @@
+module Locked_register_example
+(
+  input logic [15:0] Data_in,
+  input logic Clk,
+  input logic resetn,
+  input logic write,
+  input logic Lock,
+  input logic scan_mode,
+  input logic debug_unlocked,
+  output logic [15:0] Data_out
+);
+
+  logic lock_status;
+
+  always @(posedge Clk or negedge resetn)
+    if (~resetn) // Register is reset resetn
+      begin
+        lock_status <= 1'b0;
+      end
+    else if (Lock)
+      begin
+        lock_status <= 1'b1;
+      end
+    else if (~Lock)
+      begin
+        lock_status <= lock_status
+      end
+  always @(posedge Clk or negedge resetn)
+    if (~resetn) // Register is reset resetn
+      begin
+        Data_out <= 16'h0000;
+      end
+    else if (write & (~lock_status | scan_mode | debug_unlocked) ) // Register protected by Lock bit input, overrides supported for scan_mode & debug_unlocked
+      begin
+        Data_out <= Data_in;
+      end
+    else if (~write)
+      begin
+        Data_out <= Data_out;
+      end
+endmodule
---
+++ example_vulnerabilities/cwe-1280.sv
@@ -0,0 +1,22 @@
+module foo_bar(data_out, usr_id, data_in, clk, rst_n);
+  output logic [7:0] data_out;
+  input logic [2:0] usr_id;
+  input logic [7:0] data_in;
+  input logic clk, rst_n;
+
+  logic grant_access;
+  always @ (posedge clk or negedge rst_n)
+    begin
+      if (!rst_n)
+        data_out = 0;
+      else begin
+        //here in lies the vulnerability
+        //grant access allows data_out to be written to if
+        //usr_id = 4, but grant_access is written to in a different
+        //cycle than data_out due to the use of blocking assignments
+        //could be fixed by reversing the order of assignments,
+        data_out = (grant_access) ? data_in : data_out;
+        grant_access = (usr_id == 3'h4) ? 1'b1 : 1'b0;
+      end
+    end
+endmodule 
