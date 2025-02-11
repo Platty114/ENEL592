02c823b833af0e5c4ecc8e5ea51697e15e5bf1b6
Platty114 <jamesplatt02@gmail.com>
2025-02-10 22:15:45 -0700

    vuln cwe-1262

---
+++ src/rtl/example_vulnerabilities/cwe-1262/cwe-1262_patched.sv
@@ -0,0 +1,28 @@
+module cwe_1262 (
+    input logic 	csr_we,
+    input logic 	csr_read,
+    input logic [1:0] 	priv_lvl_o,
+    input logic [11:0]	csr_addr
+);
+
+    localparam logic[1:0] EXPECTED_PRIV_LEVEL = 2'b11;
+    localparam logic[11:0] PROTECTED_REG = 12'h064;
+
+    always_comb begin : priv_check 
+	// -----------------
+        // Privilege Check
+        // -----------------
+        // if we are reading or writing, check for the correct privilege level this has
+        // precedence over interrupts
+        if (csr_we || csr_read) begin
+	    //checks if the cpu is running at the correct priv level, if not,
+	    //an exception should be raise. The vulnerability exists in that
+	    //there is a register which is excluded from this check, even
+	    //though it should be protected
+            if ((priv_lvl_o & EXPECTED_PRIV_LEVEL != EXPECTED_PRIV_LEVEL)) begin   
+		//raise excetption
+            end
+        end	
+    end
+
+endmodule
